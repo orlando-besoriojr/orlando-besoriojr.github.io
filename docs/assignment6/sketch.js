456d4466c19f65bab80f73e71b8c3ce3935fd2f6
Orlando Besorio Jr <59715881+orlando-besoriojr@users.noreply.github.com>
2021-11-03 13:34:24 -0700

    Add files via upload

--- docs/assignment6/sketch.js
+++ docs/assignment6/sketch.js
@@ -1,21 +1,51 @@
+let img;
+function preload() {
+  img = loadImage('earth.png');
+}
+
+
 function setup() {
-    createCanvas(windowWidth, windowHeight);
+    canvas = createCanvas(windowWidth, windowHeight);
     noStroke();
+    rectMode(CENTER);
     loadJSON("http://api.open-notify.org/astros.json", dataReceived);
 }
 
 function dataReceived(data) {
-    background(240);
+    var number = data['number'];
+    var x = 400;
+    var y = 0;
+
+    image(img, 0, 0);
+    image(img, 50, 0, 40, 20, 50, 50, 50, 50);
+
+
     translate(width/2, height/2);
-    for(var i = 0; i < data.number; i++) {
-        push();
-        rotate(TWO_PI* i / 8);
+    for(var i = 0; i < number; i++) {
+        rotate(TWO_PI / 10);
         fill(random(255), random(255), random(255));
-        ellipse(300, 0, 100, 100);
-        push();
+        ellipse(x, y, 175, 175);
     }
+    data['people'].forEach(function (d) {
+        rotate(TWO_PI / 10);
+        fill('black');
+        text(d['name'], 450, 50, 175, 175);
+    });
+    data['people'].forEach(function (d) {
+        rotate(TWO_PI / 10);
+        fill('black');
+        text(d['craft'], 450, 100, 175, 175);
+    });
+}
+
+function mouseClicked() {
 }
 
 function draw() {
-
+    canvas.style(`transform: rotate(${frameCount}deg)`)
+    rotate(-radians(frameCount/200))
+    if(!mousePressed) {
+        frameCount = 0;
+    }
+    return false;
 }
