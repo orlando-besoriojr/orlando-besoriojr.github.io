a20fa1b7668f646114699171227870ec06661b21
Orlando Besorio Jr <59715881+orlando-besoriojr@users.noreply.github.com>
2021-11-03 10:48:26 -0700

    Add files via upload

--- docs/assignment6/sketch.js
+++ docs/assignment6/sketch.js
@@ -1,33 +1,27 @@
 let s13;
 let iss;
 
-var angle = 0;
-var scalar = windowHeight/2;
-varStartX = windowWidth;
-varStartY = windowHeight;
-
 function preload() {
     s13 = loadImage('s13.jpg');
-    iss = loadImage('iss.jpg');
+    iss = loadImage('iss.png');
 }
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
     noStroke();
-    angleMode(DEGREES);
     loadJSON("http://api.open-notify.org/astros.json", dataReceived);
 }
 
 function dataReceived(data) {
+    for (let i = 0; i < data['craft'].number; i++) {
+        fill(random(255), random(255), random(255));
+        ellipse(random(width),random(height),150,150, 50);
+  }
+
     data['people'].forEach(function (d) {
         $('#craft').append('<li>' + d['craft']+ '</li>');
     });
 }
 
 function draw() {
-    var x = startX + scalar * cos(angle);
-    var y = startY + scalar * sin(angle);
-    image(s13, x, y, 100);
-    angle++;
-    image(iss, 500, 500)
 }
