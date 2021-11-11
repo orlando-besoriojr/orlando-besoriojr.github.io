c093b7da577a49d116e0ff2d895b2134e9c7420c
Orlando Besorio Jr <59715881+orlando-besoriojr@users.noreply.github.com>
2021-11-10 17:57:14 -0800

    FIXEDDDDD

--- docs/assignment6/sketch.js
+++ docs/assignment6/sketch.js
@@ -13,20 +13,20 @@ function dataReceived(data) {
     translate(width/2, height/2);
     // Set circle representing each person.
     for(var i = 0; i < number; i++) {
-        rotate(TWO_PI / 10);
+        rotate(TWO_PI / number);
         fill(random(255), random(255), random(255));
         ellipse(400, 0, 175, 175);
     }
     // Get and set name.
     data['people'].forEach(function (d) {
-        rotate(TWO_PI / 10);
-        fill('black');
+        rotate(TWO_PI / number);
+        fill('white');
         text(d['name'], 450, 50, 175, 175, 100, 100);
     });
     // Get and set craft
     data['people'].forEach(function (d) {
-        rotate(TWO_PI / 10);
-        fill('black');
+        rotate(TWO_PI / number);
+        fill('white');
         text(d['craft'], 450, 100, 175, 175);
     });
 }
