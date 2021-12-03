11eda8870a85d54361c36ec457f73ef51c612f28
Orlando Besorio Jr <59715881+orlando-besoriojr@users.noreply.github.com>
2021-12-03 13:14:31 -0800

    Add files via upload

--- docs/finalproject/sketch.js
+++ docs/finalproject/sketch.js
@@ -1,22 +1,98 @@
-/* form input to put into an object "pin" */
-var song;
-var artist;
-var genre;
-var canvas = document.getElementById("canvas");
+// var song;
+// var artist;
 
-/* grabs form input */
-function info() {
-  song = document.getElementById("Song").value;
-  artist = document.getElementById("Artist").value;
-  genre = document.getElementById("Genre").value;
+// function setInfo() {
+//   song = document.getElementById("Song").value;
+//   artist = document.getElementById("Artist").value;
+// }
+
+// function test() {
+//   alert(song);
+//   alert(artist);
+// }
+
+//   display() {
+//     fill('black');
+//     ellipse(400, 400, 100, 100);
+//   }
+// }
+
+function addTo(list) {
+  var text = "";
+  var input = document.querySelectorAll("input[type=text]");
+  var link = document.createElement("a");
+  for (var i = 0; i < input.length; i++) {
+    if(i == 0) {
+      text += input[i].value + ", ";
+    }
+    else if (i == 1) {
+      text += "by " + input[i].value;
+    }
+    else {
+      text += " || " + input[i].value;
+    }
+  }
+  var li = document.createElement("li");
+  li.classList.add("list-group-item");
+  var node = document.createTextNode(text);
+  li.appendChild(node);
+  document.getElementById("list").appendChild(li);
 }
 
-/* tests if it takes form input correctly */
-function test() {
-  alert(song);
-  alert(artist);
-  alert(genre);
-}
+
+
+// window.addEventListener("load", () => {
+//   let canvas = document.getElementById("canvas");
+//   let ctx = canvas.getContext("2d");
+
+//   canvas.height = window.innerHeight;
+//   canvas.width = window.innerWidth;
+
+//   class Circle {
+//     constructor(xpos, ypos, radius, color) {
+//       this.xpos = xpos;
+//       this.ypos = ypos;
+//       this.radius = radius;
+//       this.color = color;
+//     }
+
+//     draw(ctx) {
+//       ctx.beginPath();
+//       ctx.arc(this.xpos, this.ypos, this.radius, 0, Math.PI*2, false);
+//       ctx.stroke();
+//       ctx.closePath();
+//       ctx.fillText(song, this.xpos, this.ypos);
+//     }
+
+//   }
+//   let all_circles = [];
+
+//   let createCircle = function(circle) {
+//     circle.draw(ctx);
+//   }
+  
+//   for(var numbers = 0; numbers < 10; numbers++) {
+//     let random_x = Math.random()*window.innerWidth;
+//     let random_y = Math.random()*window.innerHeight;
+//     let mc = new Circle(random_x, random_y, 50, "black");
+//     all_circles.push(mc);
+//     createCircle(all_circles[numbers]);
+//   }
+// });
+  // let all_circles = [];
+
+  // let createCircle = function(circle) {
+  //   circle.draw(context);
+  // }
+  
+  // for(var numbers = 0; numbers < 10; numbers++) {
+  //   let my_circle = new Circle(100, 100, 50, "black");
+  //   all_circles.push(my_circle);
+  //   createCircle(all_circles[numbers]);
+  // }
+
+
+
 
 /* Pin class to make objects from and draw out as input is accepted */
 // class Pin {
